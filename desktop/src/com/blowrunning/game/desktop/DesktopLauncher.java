c7984876d79cbf2158936529d37c4a2d68b4df51
mameaw14 <pinyapornster@gmail.com>
2016-12-01 19:41:52 +0700

    :tada: first commit

---
+++ desktop/src/com/blowrunning/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blowrunning.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blowrunning.game.Blowrunning;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Blowrunning(), config);
+	}
+}
