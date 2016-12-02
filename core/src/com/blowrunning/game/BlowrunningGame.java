eaae240ca73012ee84ed7f44dc256f42e6cb3eb3
mameaw14 <pinyapornster@gmail.com>
2016-12-02 11:00:24 +0700

    gamescreen

---
+++ core/src/com/blowrunning/game/BlowrunningGame.java
@@ -0,0 +1,24 @@
+package com.blowrunning.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class BlowrunningGame extends Game {
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+    setScreen(new GameScreen(this));
+	}
+
+	@Override
+	public void render () {
+    super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
