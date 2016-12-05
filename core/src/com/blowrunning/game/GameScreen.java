02cf7a44fb36c7c1da32c06853e30f19fc04201a
natachit <natachit@hotmail.com>
2016-12-05 14:37:53 +0700

    show big runner

--- core/src/com/blowrunning/game/GameScreen.java
+++ core/src/com/blowrunning/game/GameScreen.java
@@ -23,7 +23,7 @@ public class GameScreen extends ScreenAdapter {
 	@Override
 	public void render (float delta) {
     input(delta);
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor((float)204/256, (float)102/256, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
     world.render(delta);
--- core/src/com/blowrunning/game/Runner.java
+++ core/src/com/blowrunning/game/Runner.java
@@ -6,8 +6,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Runner implements Entity {
+    private int pos;
     private Vector2 position;
-    Sprite sprite;
+    Sprite sprite1, sprite2;
     SpriteBatch batch;
     Texture runnerImg;
     
@@ -17,14 +18,17 @@ public class Runner implements Entity {
         this.batch = BlowrunningGame.batch;
         if (number == 1) {
             runnerImg = new Texture("runner1.png");
+            pos = 35;
         }
         else {
             runnerImg = new Texture("runner2.png");
+            pos = 395;
         }
-        sprite = new Sprite(runnerImg);
-        sprite.setOriginCenter();
-        sprite.setPosition(position.x, position.y);
-        sprite.setSize(20, 20);
+        sprite1 = new Sprite(runnerImg);
+        sprite1.setOriginCenter();
+        sprite2 = new Sprite(runnerImg);
+        sprite2.setOriginCenter();
+        sprite1.setSize(20, 20);
     }
     
     
@@ -33,8 +37,10 @@ public class Runner implements Entity {
     }
     
     public void render(float delta) {
-        sprite.setPosition(position.x, position.y);
-        sprite.draw(batch);
+        sprite1.setPosition(position.x, position.y);
+        sprite2.setPosition(pos, -50);
+        sprite1.draw(batch);
+        sprite2.draw(batch);
     }
     
     public Vector2 getPosition() {
