fb751e30875ba8e3b77d35ffc485605e296f8791
Mameaw14 <pinyapornster@gmail.com>
2016-12-05 10:20:55 +0700

    map img

--- core/src/com/blowrunning/game/Maps.java
+++ core/src/com/blowrunning/game/Maps.java
@@ -1,23 +1,27 @@
 package com.blowrunning.game;
 
-import java.util.HashMap;
-import java.util.Hashtable;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Maps implements Entity {
+  Map<Integer, Sprite> map;
   
   Maps() {
-    Map<Integer, String> map = new TreeMap<Integer, String>();
-    map.put(100, "Grass");
-    map.put((int)200, "Grass");
-    map.put((int)140, "150");
+    Texture grass = new Texture("grass.png");
+    Texture flag = new Texture("flag.png");
+    map = new TreeMap<Integer, Sprite>();
+    map.put(100, new Sprite(grass));
+    map.put(200, new Sprite(grass));
+    map.put(140, new Sprite(flag));
     for(int key: map.keySet()) {
       System.out.println(key + " - " + map.get(key));
     }
   }
   @Override
   public void render(float delta) {
-    
+    Sprite sprite = map.get(100);
+    sprite.setPosition(BlowrunningGame.WIDTH/3, 500);
   }
 }
