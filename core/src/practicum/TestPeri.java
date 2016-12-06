0bf6b59f01cdd561f4c5ea64b65ea7c7ca89ccf0
mameaw14 <pinyapornster@gmail.com>
2016-12-06 16:10:24 +0700

    getspeed 2 player

--- core/src/practicum/TestPeri.java
+++ core/src/practicum/TestPeri.java
@@ -1,13 +1,13 @@
 package practicum;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import org.usb4java.Device;
 
 public class TestPeri
 {
     McuWithPeriBoard peri;
-    LinkedList<Boolean> q;
-    int s;
+    LinkedList<Boolean>[] p;
     public TestPeri()
     {
         McuBoard.initUsb();
@@ -29,7 +29,13 @@ public class TestPeri
             System.out.format("** Manufacturer: %s\n", peri.getManufacturer());
             System.out.format("** Product: %s\n", peri.getProduct());
             
-            q = new LinkedList<Boolean>();
+            p[0] = new LinkedList<Boolean>(Arrays.asList(false, false, false, 
+                false, false, false, false, false, false, false, false, false, 
+                false, false, false, false, false, false, false, false, false, 
+                false, false, false, false));
+            p[1] = new LinkedList<Boolean>(Arrays.asList(false, false, false, 
+                false, false, false, false, false, false, false, false, false, 
+                false, false, false, false, false));
         }
         catch (Exception e)
         {
@@ -37,23 +43,30 @@ public class TestPeri
         }
     }
     
-    int sum() {
-      if(q.size() > 25) q.removeFirst();
+    int sum(int i) {
+      i = i - 1;
+      if(p[i].size() > 25) p[i].removeFirst();
       int sum = 0;
-      for (boolean x: q) {
+      for (boolean x: p[i]) {
         if(x) sum++;
       }
       return sum;
     }
-    public float getSpeed() {
-      return sum() / 2F;
+    
+    public float getSpeed(int i) {
+      return sum(i) / 5F;
     }
+    
     public void update() {
-      boolean sw = peri.getLowerSound();
-      System.out.println(sw);
-      q.add(sw);
-      s = sum();
+      boolean sw1 = peri.getLowerSound(1);
+      p[0].add(sw1);
+      sum(0);
+      
+      boolean sw2 = peri.getLowerSound(2);
+      p[1].add(sw2);
+      sum(1);
     }
+    
     public void dispose() {
       McuBoard.cleanupUsb();
     }
